b3856d601b90df84ded592454f17d19ece22d759
Myron-Mac <MYRon_TO@outlook.com>
2023-07-09 14:50:19 +0800

    Added code-action

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -3,7 +3,15 @@ return {
         'mbbill/undotree',
         lazy = true,
         keys = {
-            {"cud", ":UndotreeToggle<CR>", "n"},
+            { "cud", ":UndotreeToggle<CR>", "n" },
+        },
+    },
+    {
+        'weilbith/nvim-code-action-menu',
+        lazy = true,
+        cmd = 'CodeActionMenu',
+        keys = {
+            { "<leader>a", ":CodeActionMenu<CR>", "n" },
         },
     },
 }
