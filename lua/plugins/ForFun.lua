8f80ac57ae71a0cf016a7bf137e38e5901f11895
Myron_Arch <Myron_TO@outlook.com>
2023-07-25 13:48:18 +0800

    Fixed a key conflict

--- lua/plugins/ForFun.lua
+++ lua/plugins/ForFun.lua
@@ -4,11 +4,15 @@ return {
     keys = {
         -- "<leader>fmr",
         -- "<leader>fgl",
-        "<leader>ff",
+        "<leader>fr",
+    },
+    cmd = {
+        "CellularAutomaton make_it_rain",
+        "CellularAutomaton game_of_life",
     },
     config = function()
         -- vim.keymap.set("n", "<leader>fmr", "<cmd>CellularAutomaton make_it_rain<CR>")
         -- vim.keymap.set("n", "<leaderr>fgl", "<cmd>CellularAutomaton game_of_life<CR>")
-        vim.keymap.set("n", "<leader>ff", "<cmd>CellularAutomaton make_it_rain<CR>")
+        vim.keymap.set("n", "<leader>fr", "<cmd>CellularAutomaton make_it_rain<CR>")
     end,
 }
