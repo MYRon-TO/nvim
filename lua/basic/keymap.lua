0f309ea8255bf12616c333452da5724473611ad3
Myron_Arch <Myron_TO@outlook.com>
2024-03-23 14:12:59 +0800

    Improved key binding

--- lua/basic/keymap.lua
+++ lua/basic/keymap.lua
@@ -10,50 +10,46 @@ vim.g.mapleader = ' '
 map('n', 'c<space>', 'o<Esc>', opts)
 map('n', 'cd', '0D', opts)
 map('n', 'cnh', ':nohl<CR>', opts_si)
-map('n', 'cfm', ':lua vim.lsp.buf.format({async = false })<CR>', opts)
+-- map('n', 'cfm', ':lua vim.lsp.buf.format({async = false })<CR>', opts)
 
 -- quick move
-map('n', '<c-j>', '5j', opts)
-map('n', '<c-k>', '5k', opts)
-map('n', '<c-h>', '5h', opts)
-map('n', '<c-l>', '5l', opts)
-map('v', '<c-j>', '5j', opts)
-map('v', '<c-k>', '5k', opts)
-map('v', '<c-h>', '5h', opts)
-map('v', '<c-l>', '5l', opts)
-map('x', '<c-j>', '5j', opts)
-map('x', '<c-k>', '5k', opts)
-map('x', '<c-h>', '5h', opts)
-map('x', '<c-l>', '5l', opts)
+vim.keymap.set({ "n", "v", "x", }, '<c-j>', '10j', opts)
+vim.keymap.set({ "n", "v", "x", }, '<c-k>', '10k', opts)
+vim.keymap.set({ "n", "v", "x", }, '<c-h>', '10h', opts)
+vim.keymap.set({ "n", "v", "x", }, '<c-l>', '10l', opts)
 
 -- split window
-map('n', 'csh', ':set nosplitright<CR>:vsplit<CR>', opts_si)
-map('n', 'csl', ':set splitright<CR>:vsplit<CR>', opts_si)
-map('n', 'csj', ':set splitbelow<CR>:split<CR>', opts_si)
-map('n', 'csk', ':set nosplitright<CR>:split<CR>', opts_si)
-map('n', '<LEADER>h', '<C-w>h', opts_si)
-map('n', '<LEADER>l', '<C-w>l', opts_si)
-map('n', '<LEADER>j', '<C-w>j', opts_si)
-map('n', '<LEADER>k', '<C-w>k', opts_si)
-map('n', '<LEADER><left>', ':vertical resize-5<CR>', opts_si)
-map('n', '<LEADER><right>', ':vertical resize+5<CR>', opts_si)
-map('n', '<LEADER><down>', ':res +5<CR>', opts_si)
-map('n', '<LEADER><up>', ':res -5<CR>', opts_si)
+map('n', '<c-w>sh', ':set nosplitright<CR>:vsplit<CR>', opts_si)
+map('n', '<c-w>sl', ':set splitright<CR>:vsplit<CR>', opts_si)
+map('n', '<c-w>sj', ':set splitbelow<CR>:split<CR>', opts_si)
+map('n', '<c-w>sk', ':set nosplitright<CR>:split<CR>', opts_si)
+-- map('n', '<c-w>h', '<C-w>h', opts_si)
+-- map('n', '<c-w>l', '<C-w>l', opts_si)
+-- map('n', '<c-w>j', '<C-w>j', opts_si)
+-- map('n', '<c-w>k', '<C-w>k', opts_si)
+map('n', '<c-w><left>', ':vertical resize-5<CR>', opts_si)
+map('n', '<c-w><right>', ':vertical resize+5<CR>', opts_si)
+map('n', '<c-w><down>', ':res +5<CR>', opts_si)
+map('n', '<c-w><up>', ':res -5<CR>', opts_si)
 
 -- tab
-map('n', 'ct', ':tabe<CR>', opts_si)
-map('n', '<LEADER>H', ':-tabnext<CR>', opts_si)
-map('n', '<LEADER>L', ':+tabnext<CR>', opts_si)
-map('n', '<LEADER>J', ':-tabmove<CR>', opts_si)
-map('n', '<LEADER>K', ':+tabmove<CR>', opts_si)
+map('n', '<c-w>t', ':tabe<CR>', opts_si)
+map('n', '<c-w>J', ':-tabnext<CR>', opts_si)
+map('n', '<c-w>K', ':+tabnext<CR>', opts_si)
+map('n', '<c-w>H', ':-tabmove<CR>', opts_si)
+map('n', '<c-w>L', ':+tabmove<CR>', opts_si)
 
 -- buffer
-map('n', '<LEADER>F', ':bfirst<CR>', opts_si)
-map('n', '<LEADER>B', ':blast<CR>', opts_si)
+-- map('n', '<LEADER>F', ':bfirst<CR>', opts_si)
+-- map('n', '<LEADER>B', ':blast<CR>', opts_si)
 
 -- stay in visual mode after indent
 map('v', '<', '<gv', opts)
 map('v', '>', '>gv', opts)
+
+-- stay in visual mode after + and -
+map('v', '<c-a>', '<c-a>gv', opts)
+map('v', '<c-x>', '<c-x>gv', opts)
 
 -- move selected line / block of text in visual mode
 map('v', 'J', ":m '>+1<CR>gv=gv", opts)
