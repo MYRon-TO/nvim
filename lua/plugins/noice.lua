3c5947521fe3fcebff5a0c29fc0482bf04a03830
Myron-Mac <MYRon_TO@outlook.com>
2024-05-18 20:27:29 +0800

    Added trouble.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,58 +1,60 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    {
-      "rcarriga/nvim-notify",
-      opts = {
-        background_colour = "NotifyBackground",
-        fps = 60,
-        icons = {
-          DEBUG = "",
-          ERROR = "",
-          INFO = "",
-          TRACE = "✎",
-          WARN = ""
-        },
-        level = 2,
-        minimum_width = 50,
-        render = "default",
-        stages = "slide",
-        timeout = 3000,
-        top_down = true
-      },
-      config = function()
-        local opt = { silent = true, noremap = true }
-        vim.keymap.set("n", "<leader><leader>", require("notify").dismiss, opt)
-      end,
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
     },
-  },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      {
+        "rcarriga/nvim-notify",
+        opts = {
+          background_colour = "NotifyBackground",
+          fps = 60,
+          icons = {
+            DEBUG = "",
+            ERROR = "",
+            INFO = "",
+            TRACE = "✎",
+            WARN = ""
+          },
+          level = 2,
+          minimum_width = 50,
+          render = "default",
+          stages = "slide",
+          timeout = 3000,
+          top_down = true
+        },
+        config = function()
+          local opt = { silent = true, noremap = true }
+          vim.keymap.set("n", "<leader><leader>", require("notify").dismiss, opt)
+        end,
+      },
+    },
 
-  config = function()
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        -- override = {
-        --   ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        --   ["vim.lsp.util.stylize_markdown"] = true,
-        --   ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        -- },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false,        -- use a classic bottom cmdline for search
-        command_palette = true,       -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-      },
-    })
-  end
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          -- override = {
+          --   ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          --   ["vim.lsp.util.stylize_markdown"] = true,
+          --   ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          -- },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false,        -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        },
+      })
+    end
+  }
 }
